0335b8c4abccb6be3e84a1a5c82c6a91643e8bf7
Ivan Fomenko <bahhekk@mail.ru>
2022-03-11 17:04:16 +0500

    new git ignore

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y;
+
+    cout << "Enter X: "; cin>>x;
+    cout << "Enter Y: "; cin>>y;
+
+    cout << "X + Y = "<<x+y<<endl;
+    return 0;
+}
